c9bdd137c230fe0b729faa392efef8dc8e93b5dd
Nick Inman <nick@theoatmeal.com>
2018-12-06 04:11:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
+require'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -9,5 +11,46 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name 
+    song 
+  #binding.pry
+  end 
+  
+  def self.create_by_name(name)
+    song = Song.create
+    song.name = name 
+    song 
+  end
+  
+  def self.find_by_name(name)
+    @@all.find do |x|
+      x.name == name
+    end
+  end
+  
+  def self.find_or_create_by_name(name)
+    create_by_name(name)
+    find_by_name(name)
+  end 
+  
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(name)
+    data = name.split(/[-.]/)
+    song = Song.new
+    song.name = data[1].
+    #binding.pry
+    song 
+  end
 end
